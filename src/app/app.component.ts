a3f3ec98eca710b7a274a8f2e0c692ad2ec40a65
Mohammad Itani <mohammad.itani1999@hotmail.com>
2024-10-05 13:27:13 +0300

    side bar initial design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component, TemplateRef } from '@angular/core';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { Component, inject, TemplateRef } from '@angular/core';
+import { NgbDatepickerModule, NgbOffcanvas, OffcanvasDismissReasons, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgbDropdownModule],
+  imports: [NgbDatepickerModule, NgbDropdownModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'BootMatTemplate';
-  constructor() {}
+  private offcanvasService = inject(NgbOffcanvas);
+
+  open(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, {
+      ariaLabelledBy: 'sidebar',
+      panelClass: 'container-offcanvas',
+    });
+  }
 }
