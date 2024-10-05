7765abc55feda20b05cb3a087f1b3500d24b3eb6
Mohammad Itani <mohammad.itani1999@hotmail.com>
2024-10-05 21:06:35 +0300

    added test form with search bar

--- src/app/Components/test-form/test-form.component.ts
+++ src/app/Components/test-form/test-form.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-test-form',
   standalone: true,
-  imports: [NgbNavModule],
+  imports: [NgbNavModule, FormsModule, CommonModule],
   templateUrl: './test-form.component.html',
   styleUrl: './test-form.component.scss'
 })
