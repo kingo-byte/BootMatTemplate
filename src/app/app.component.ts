f4fbf88e36d8945c5709ee49a1de26f6d23f38a6
Mohammad Itani <mohammad.itani1999@hotmail.com>
2024-10-05 17:56:15 +0300

    main design updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component} from '@angular/core';
 import { NavbarComponent } from "./Components/navbar/navbar.component";
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from './Components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    NavbarComponent,
-    FooterComponent,
-     RouterModule],
+  imports: [NavbarComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
