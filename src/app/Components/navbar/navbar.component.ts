881e686e6fc699b5fc86ab5c92e78702ea440e5d
Mohammad Itani <mohammad.itani1999@hotmail.com>
2024-10-06 11:05:24 +0300

    enhanced side bar

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -2,17 +2,20 @@ import { Component, inject, TemplateRef } from '@angular/core';
 import { NgbDatepickerModule, NgbOffcanvas, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgbDatepickerModule, NgbDropdownModule, NavbarComponent, NavbarComponent, FontAwesomeModule],
+  imports: [NgbDatepickerModule, NgbDropdownModule, NavbarComponent, NavbarComponent, FontAwesomeModule, NgbCollapseModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   private offcanvasService = inject(NgbOffcanvas);
   faBars = faBars;
+  isDashboardCollapsed = true;
 
   open(content: TemplateRef<any>) {
     this.offcanvasService.open(content, {
