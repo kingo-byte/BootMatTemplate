5814d4355b492381da95a26c7ed28f4cabb5b492
Mohammad Itani <mohammad.itani1999@hotmail.com>
2024-10-13 09:11:39 +0300

    fixed minor route thing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,15 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    {
+     {
         path: 'auth',
         loadChildren: () =>
           import('./Modules/auth/auth.module').then((m) => m.AuthModule),
      },
-    {
+     {
       path: 'main',
       loadChildren: () =>
         import('./Modules/main/main.module').then((m) => m.MainModule),
-    },
-    {path: '**', redirectTo: 'main'}    
+     },
+    {path: '**', redirectTo: 'auth'}    
 ];
